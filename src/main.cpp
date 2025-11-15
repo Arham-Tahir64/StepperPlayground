0a1fcd662d484e968112c401dbe22f0547ce9bd2
Arham Tahir <arhamt13@gmail.com>
2025-11-15 13:44:21 -0700

    Controlling stepper 1 with x, stepper 2 with y

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,20 @@
 #include <AccelStepper.h>
 
 const int JOY_X = A0;
+const int JOY_Y = A1;
 
 const int DIR_PIN_2 = 10;
 const int STEP_PIN_2 = 9;
 const int EN_PIN_2 = 8;
 
+const int EN_PIN_1 = 4;
+const int STEP_PIN_1 = 5;
+const int DIR_PIN_1 = 6;
+
 const int SPEED = 400;
 
-AccelStepper myStepper(AccelStepper::DRIVER, STEP_PIN_2, DIR_PIN_2);
+AccelStepper StepperX(AccelStepper::DRIVER, STEP_PIN_2, DIR_PIN_2);
+AccelStepper StepperY(AccelStepper::DRIVER, STEP_PIN_1, DIR_PIN_1);
 
 
 // put function declarations here:
@@ -21,14 +27,24 @@ void setup() {
   pinMode(EN_PIN_2, OUTPUT);
   digitalWrite(EN_PIN_2, LOW); // Enable the stepper driver
 
-  myStepper.setMaxSpeed(1000);
-  myStepper.setAcceleration(500);
+  pinMode(EN_PIN_1, OUTPUT);
+  digitalWrite(EN_PIN_1, LOW); // Enable the stepper driver
+
+  StepperX.setMaxSpeed(1000);
+  StepperX.setAcceleration(500);
+  StepperY.setMaxSpeed(1000);
+  StepperY.setAcceleration(500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int xVal = analogRead(JOY_X);
   int stepsToMove = map(xVal, 0, 1023, -SPEED, SPEED);
-  myStepper.setSpeed(stepsToMove);
-  myStepper.runSpeed();
+  StepperX.setSpeed(stepsToMove);
+  StepperX.runSpeed();
+
+  int yVal = analogRead(JOY_Y);
+  int stepsToMove1 = map(yVal, 0, 1023, -SPEED, SPEED);
+  StepperY.setSpeed(stepsToMove1);
+  StepperY.runSpeed();
 }
